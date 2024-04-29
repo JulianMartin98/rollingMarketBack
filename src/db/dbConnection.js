c40c851ec079a4fee303ce0bf38cc6d85807fa95
Alvaro D. <alvarodeguer@gmail.com>
2024-04-29 12:23:58 -0300

    Creado archivo de la conexion de la DB

---
+++ src/db/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { CONNECTION_STRING } from "../config/config.js";
+
+
+mongoose.connect(CONNECTION_STRING);
+
+
+mongoose.connection.on("connected", () => {
+    console.log("Conectado a la base de datos de MongoDB")
+
+})
+
+mongoose.connection.on("error", (error) => {
+    console.error("Error de conexión a MongoDB: ", error)
+})
