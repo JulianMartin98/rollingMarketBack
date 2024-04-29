d7219c8ba12681876d4512ff2974b0d912484ac3
Alvaro D. <alvarodeguer@gmail.com>
2024-04-29 19:15:20 -0300

    Actualizado campo image del modelo

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -36,6 +36,7 @@ const productSchema = new Schema(
 		},
 		image: {
 			type: String,
+			default: "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg",
 		},
 	},
 	{
