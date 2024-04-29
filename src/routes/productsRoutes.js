785418f587bcbdc4abc2a03a723a509c1391815d
Alvaro D. <alvarodeguer@gmail.com>
2024-04-29 19:22:59 -0300

    Creadas todas las rutas para el funcionamiento completo del CRUD de productos

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
-import { GetAllProducts,GetProductById,CreateProduct,DeleteProduct,UpdateProduct } from "../controllers/ProductController.js";
+import { GetAllProducts, CreateProduct, DeleteProduct, UpdateProduct, GetProductbyCategory } from "../controllers/ProductController.js";
 
 
 
 const router = express.Router();
 
-router.get("/",GetAllProducts);
-router.get("/:id",GetProductById);
+router.get("/", GetAllProducts);
 router.post("/create",CreateProduct);
 router.delete("/delete/:id",DeleteProduct);
 router.put("/update/:id",UpdateProduct);
+router.get("/category/:category",GetProductbyCategory);
 
 
 export default router; 
