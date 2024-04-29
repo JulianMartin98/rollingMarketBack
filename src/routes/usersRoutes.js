597d9d20675731d0bf9d2333c4ad0dd568a5e151
Julian Martin <julianmartin19@outlook.com>
2024-04-29 14:08:42 -0300

    metodos con token testeados y funcionando correctamente

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { CreateUser, DeleteUser, GetAllUsers, GetUserById, UpdateUser, LoginUser } from "../controllers/userController.js";
-
+import comprobarJWT from "../middlewares/verifyJWT.js";
 
 
 const router = express.Router();
 
-router.get("/",GetAllUsers);
-router.get("/:id",GetUserById);
-router.post("/create",CreateUser);
-router.delete("/delete/:id",DeleteUser);
-router.put("/update/:id",UpdateUser);
+router.get("/",comprobarJWT,GetAllUsers);
+router.get("/:id",comprobarJWT,GetUserById);
+router.post("/create",comprobarJWT,CreateUser);
+router.delete("/delete/:id",comprobarJWT,DeleteUser);
+router.put("/update/:id",comprobarJWT,UpdateUser);
 router.post("/login",LoginUser);
 
 
